e337f2b73aa848b77d49625e8e1cc4041db9a565
彭晓鸿 <pengxiaohong@weiqianjin.cn>
2018-03-08 18:23:24 +0800

    pxh

--- rapid-core/src/main/java/org/rapid/core/config/DistributeConfig.java
+++ rapid-core/src/main/java/org/rapid/core/config/DistributeConfig.java
@@ -6,8 +6,4 @@ public abstract class DistributeConfig implements Serializable {
 
 	private static final long serialVersionUID = -1503288535663397135L;
 
-	/**
-	 * 默认的配置
-	 */
-	public abstract <T extends DistributeConfig> T defaultConfiguration();
 }
