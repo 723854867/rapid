87a320f764af2882fbfa8488a345ec65d2de2405
lynn <723854867@qq.com>
2018-03-21 20:56:09 +0800

    lynn

--- rapid-util/src/test/java/org/rapid/util/BeanUtilTest.java
+++ rapid-util/src/test/java/org/rapid/util/BeanUtilTest.java
@@ -1,15 +1,16 @@
 package org.rapid.util;
 
-import org.rapid.util.io.SFTPConnection;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.rapid.util.io.ZipWriter;
 
 public class BeanUtilTest {
 
 	public static void main(String[] args) throws Exception {
-		SFTPConnection connection = new SFTPConnection();
-		connection.setPort(50022);
-		connection.setHost("222.73.39.37");
-		connection.setPriKeyFile(SFTPConnection.class.getResource("/conf/id_rsa").getFile());
-		connection.setUsername("200004595271");
-		connection.init();
+		ZipWriter writer = new ZipWriter(512, new FileOutputStream(new File("E:/wqj.zip")));
+		writer.wrap("wqj/test.jpg", new FileInputStream(new File("E:/1.jpg")));
+		writer.close();
 	}
 }
