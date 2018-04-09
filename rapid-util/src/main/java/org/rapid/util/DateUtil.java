94fa0b32f7fb93900bfe1ace3fef8cdb6c99e5e2
lynn <723854867@qq.com>
2018-04-09 19:29:13 +0800

    lynn

--- rapid-util/src/main/java/org/rapid/util/DateUtil.java
+++ rapid-util/src/main/java/org/rapid/util/DateUtil.java
@@ -25,6 +25,7 @@ public class DateUtil {
 	public static final String HHmmss					= "HHmmss";
 	public static final String HH_mm_ss					= "HH:mm:ss";
 	public static final String yyyyMMdd					= "yyyyMMdd";
+	public static final String yyyyMMddHH				= "yyyyMMddHH";
 	public static final String yyyy$MM$dd				= "yyyy/MM/dd";
 	public static final String yyyy_MM_dd				= "yyyy-MM-dd";
 	public static final String yyyyMMddHHmmss			= "yyyyMMddHHmmss";
